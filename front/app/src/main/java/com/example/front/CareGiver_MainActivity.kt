9ab7087158c858c14f0f2a7a9fc34a8b08db8baf
simple0ee <whdydgus2001@kyonggi.ac.kr>
2025-05-08 11:27:03 +0900

    1. activity_main.xml -> activity_caregiver_home.xml 으로 수정

--- front/app/src/main/java/com/example/front/CareGiver_MainActivity.kt
+++ front/app/src/main/java/com/example/front/CareGiver_MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class CareGiver_MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // ← XML 제대로 연결되어야 함
+        setContentView(R.layout.activity_caregiver_home) // ← XML 제대로 연결되어야 함
     }
 }
